64be4c364de1204a7c957733e5f1534b8ee99804
dmitriywolf <dmitriyvolchenko3003@gmail.com>
2021-02-08 13:17:14 +0200

    start template

---
+++ src/main.js
@@ -0,0 +1,14 @@
+
+
+
+class App {
+  run = async (name = 'World') => {
+    console.log(`Hello ${name}`)
+    console.log([1, 2, [3, 4]].flat());
+  }
+}
+
+const app = new App();
+app.run()
+  .then( () => console.log('done') )
+  .catch( (err) => console.log(err))
